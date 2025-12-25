0fe4a6550aaed4c823c3a826b715f175e0778f0f
Liu von Engelbrechten <vonengelbrechten.l@northeastern.edu>
2025-12-25 15:59:26 -0500

    organized player controller

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -51,8 +51,6 @@ public class PlayerController : MonoBehaviour {
     [SerializeField] private Transform furniturePoint;
     
 
-    
-
     private void Start() {
         Cursor.lockState = CursorLockMode.Locked;
     }
@@ -92,6 +90,30 @@ public class PlayerController : MonoBehaviour {
             }
         }
 
+        HandleMovement();
+
+        //Handles player interactions depending on what they are holding.
+        if (heldPickup == null && heldBox == null && heldFurniture == null) {
+            HandleNothingInHand();
+        } else {
+            if (heldPickup != null) {
+                HandlePickupInHand();
+            }
+            if (heldBox != null) {
+                HandleBoxInHand();
+            }
+            if (heldFurniture != null) {
+                HandleFurnitureInHand();
+
+            }
+
+        }
+    }
+
+    /// <summary>
+    /// Handles all player movement including walking around and jumping.
+    /// </summary>
+    private void HandleMovement() {
         Vector2 lookinput = lookAction.action.ReadValue<Vector2>();
 
         horiRot += lookinput.x * Time.deltaTime * lookSpeed;
@@ -129,222 +151,252 @@ public class PlayerController : MonoBehaviour {
         moveAmount.y = ySpeed;
 
         charCon.Move(moveAmount * Time.deltaTime);
+    }
 
-
-        //checks for pickup
+    /// <summary>
+    /// Handles interacting with stock, box of stock, and furniture with nothing in hand.
+    /// </summary>
+    private void HandleNothingInHand() {
         Ray ray = theCam.ViewportPointToRay(new Vector3(.5f, .5f, 0f));
         RaycastHit hit;
 
-        if (heldPickup == null && heldBox == null && heldFurniture == null) {
-            if (Mouse.current.leftButton.wasPressedThisFrame) {
-                if (Physics.Raycast(ray, out hit, interactionRange, whatIsStock)) {
+        //picks an object to hold
+        if (Mouse.current.leftButton.wasPressedThisFrame) {
 
-                    heldPickup = hit.collider.GetComponent<StockObject>();
-                    heldPickup.transform.SetParent(holdPoint);
-                    heldPickup.Pickup();
+            // if the object is stock
+            if (Physics.Raycast(ray, out hit, interactionRange, whatIsStock)) {
 
-                    if (AudioManager.instance != null) {
-                        AudioManager.instance.PlaySFX(6);
-                    }
+                heldPickup = hit.collider.GetComponent<StockObject>();
+                heldPickup.transform.SetParent(holdPoint);
+                heldPickup.Pickup();
 
-                    return;
+                if (AudioManager.instance != null) {
+                    AudioManager.instance.PlaySFX(6);
                 }
 
-                if (Physics.Raycast(ray, out hit, interactionRange, whatIsStockBox)) {
-                    heldBox = hit.collider.GetComponent<StockBoxController>();
-
-                    heldBox.transform.SetParent(boxHoldPoint);
-                    heldBox.Pickup();
-
-                    if (heldBox.flap1.activeSelf == true) {
-                        heldBox.OpenClose();
-                    }
-
-                    if (AudioManager.instance != null) {
-                        AudioManager.instance.PlaySFX(1);
-                    }
-
-                    return;
-                }
-
-                if (Physics.Raycast(ray, out hit, interactionRange, whatIsCheckout)) {
-                    hit.collider.GetComponent<Checkout>().CheckoutCustomer();
-                }
+                return;
             }
 
-            if (Mouse.current.rightButton.wasPressedThisFrame) {
-                if (Physics.Raycast(ray, out hit, interactionRange, whatIsShelf)) {
-                    heldPickup = hit.collider.GetComponent<ShelfSpaceController>().GetStock();
+            // if the object is a box of stock
+            if (Physics.Raycast(ray, out hit, interactionRange, whatIsStockBox)) {
+                heldBox = hit.collider.GetComponent<StockBoxController>();
 
-                    if (heldPickup != null) {
-                        heldPickup.transform.SetParent(holdPoint);
-                        heldPickup.Pickup();
-                    }
+                heldBox.transform.SetParent(boxHoldPoint);
+                heldBox.Pickup();
 
-                    return;
-                }
-
-                if (Physics.Raycast(ray, out hit, interactionRange, whatIsStockBox)) {
-                    hit.collider.GetComponent<StockBoxController>().OpenClose();
-                }
-            }
-
-            if (Keyboard.current.eKey.wasPressedThisFrame) {
-                if (Physics.Raycast(ray, out hit, interactionRange, whatIsShelf)) {
-                    hit.collider.GetComponent<ShelfSpaceController>().StartPriceUpdate();
-                }
-            }
-
-            if (Keyboard.current.rKey.wasPressedThisFrame) {
-                if (Physics.Raycast(ray, out hit, interactionRange, whatIsFurniture)) {
-                    heldFurniture = hit.transform.GetComponent<FurnitureController>();
-
-                    heldFurniture.transform.SetParent(null, true);
-
-                    heldFurniture.MakePlaceable();
-                    heldFurniture.SetPlacementPosition(heldFurniture.transform.position);
-
-                    if (AudioManager.instance != null) {
-                        AudioManager.instance.PlaySFX(4);
-                    }
-                }
-            }
-        } else {
-            if (heldPickup != null) {
-                if (Mouse.current.leftButton.wasPressedThisFrame) {
-                    if (Physics.Raycast(ray, out hit, interactionRange, whatIsShelf)) {
-
-                        hit.collider.GetComponent<ShelfSpaceController>().PlaceStock(heldPickup);
-
-                        if (heldPickup.isPlaced == true) {
-                            heldPickup = null;
-                        }
-
-                        if (AudioManager.instance != null) {
-                            AudioManager.instance.PlaySFX(7);
-                        }
-                    }
-                }
-
-                if (Keyboard.current.fKey.wasPressedThisFrame) {
-                    if (heldPickup.info != null && heldPickup.info.cardPack != null) {
-                        Debug.Log("player pressed f with cardpack in hand");
-                    }
-                }
-
-                if (Mouse.current.rightButton.wasPressedThisFrame) {
-
-                    heldPickup.Release();
-                    heldPickup.theRB.AddForce(theCam.transform.forward * throwForce, ForceMode.Impulse);
-
-
-                    heldPickup.transform.SetParent(null);
-                    heldPickup = null;
-
-                    if (AudioManager.instance != null) {
-                        AudioManager.instance.PlaySFX(9);
-                    }
-                }
-            }
-
-            if (heldBox != null) {
-                if (Mouse.current.rightButton.wasPressedThisFrame) {
-
-                    heldBox.Release();
-                    heldBox.theRB.AddForce(theCam.transform.forward * throwForce, ForceMode.Impulse);
-
-                    heldBox.transform.SetParent(null);
-                    heldBox = null;
-
-                    if (AudioManager.instance != null) {
-                        AudioManager.instance.PlaySFX(0);
-                    }
-                }
-
-                if (Keyboard.current.eKey.wasPressedThisFrame) {
+                if (heldBox.flap1.activeSelf == true) {
                     heldBox.OpenClose();
                 }
 
-                if (Mouse.current.leftButton.wasPressedThisFrame) {
-                    if (heldBox.stockInBox.Count > 0) {
-
-                        if (Physics.Raycast(ray, out hit, interactionRange, whatIsShelf)) {
-                            heldBox.PlaceStockOnShelf(hit.collider.GetComponent<ShelfSpaceController>());
-
-                            placeStockCounter = waitToPlaceStock;
-
-                            if (AudioManager.instance != null) {
-                                AudioManager.instance.PlaySFX(7);
-                            }
-                        }
-                    } else {
-                        if (Physics.Raycast(ray, out hit, interactionRange, whatIsBin)) {
-                            Destroy(heldBox.gameObject);
-
-                            heldBox = null;
-
-                            if (AudioManager.instance != null) {
-                                AudioManager.instance.PlaySFX(10);
-                            }
-                        }
-                    }
-
-                    
+                if (AudioManager.instance != null) {
+                    AudioManager.instance.PlaySFX(1);
                 }
 
-                if (Mouse.current.leftButton.isPressed) {
-                    placeStockCounter -= Time.deltaTime;
-
-                    if (placeStockCounter <= 0) {
-                        if (Physics.Raycast(ray, out hit, interactionRange, whatIsShelf)) {
-                            heldBox.PlaceStockOnShelf(hit.collider.GetComponent<ShelfSpaceController>());
-
-                            placeStockCounter = waitToPlaceStock;
-                        }
-                    }
-                }
+                return;
             }
 
-            if (heldFurniture != null) {
+            // if the object is the checkout register
+            if (Physics.Raycast(ray, out hit, interactionRange, whatIsCheckout)) {
+                hit.collider.GetComponent<Checkout>().CheckoutCustomer();
+            }
+        }
 
-                // --- GRID-SNAPPED POSITION (Y LOCKED INSIDE) ---
-                Vector3 targetPos = furniturePoint.position;
-                heldFurniture.SetPlacementPosition(targetPos);
+        // to get stock off of the shelf
+        if (Mouse.current.rightButton.wasPressedThisFrame) {
+            if (Physics.Raycast(ray, out hit, interactionRange, whatIsShelf)) {
+                heldPickup = hit.collider.GetComponent<ShelfSpaceController>().GetStock();
 
-                // --- ROTATION (30° steps via scroll) ---
-                float scroll = Mouse.current.scroll.ReadValue().y;
-                if (scroll > 0f) {
-                    heldFurniture.RotatePlacement(1);
-                }
-                else if (scroll < 0f) {
-                    heldFurniture.RotatePlacement(-1);
-                }
-
-                // --- OVERLAP CHECK ---
-                bool overlapping = heldFurniture.IsFurnitureOverlapping();
-
-                if (overlapping) {
-                    heldFurniture.setColorRed();
-                } else {
-                    heldFurniture.setColorGreen();
+                if (heldPickup != null) {
+                    heldPickup.transform.SetParent(holdPoint);
+                    heldPickup.Pickup();
                 }
 
-                // --- PLACE ---
-                if (Mouse.current.leftButton.wasPressedThisFrame ||
-                    Keyboard.current.rKey.wasPressedThisFrame) {
+                return;
+            }
 
-                    if (!overlapping) {
-                        heldFurniture.transform.SetParent(null);
-                        heldFurniture.PlaceFurniture();
-                        heldFurniture = null;
-                    }
+            if (Physics.Raycast(ray, out hit, interactionRange, whatIsStockBox)) {
+                hit.collider.GetComponent<StockBoxController>().OpenClose();
+            }
+        }
+
+        // to update the price of a shelf 
+        if (Keyboard.current.eKey.wasPressedThisFrame) {
+            if (Physics.Raycast(ray, out hit, interactionRange, whatIsShelf)) {
+                hit.collider.GetComponent<ShelfSpaceController>().StartPriceUpdate();
+            }
+        }
+
+        // to rotate or move a piece of furniture
+        if (Keyboard.current.rKey.wasPressedThisFrame) {
+            if (Physics.Raycast(ray, out hit, interactionRange, whatIsFurniture)) {
+                heldFurniture = hit.transform.GetComponent<FurnitureController>();
+
+                heldFurniture.transform.SetParent(null, true);
+
+                heldFurniture.MakePlaceable();
+                heldFurniture.SetPlacementPosition(heldFurniture.transform.position);
+
+                if (AudioManager.instance != null) {
+                    AudioManager.instance.PlaySFX(4);
+                }
+            }
+        }
+    }
+
+    /// <summary>
+    /// Handles what you can do while holding stock in your hand.
+    /// This includes placing it on shelf or throwing it.
+    /// </summary>
+    private void HandlePickupInHand() {
+        Ray ray = theCam.ViewportPointToRay(new Vector3(.5f, .5f, 0f));
+        RaycastHit hit;
+        // placing stock onto a shelf
+        if (Mouse.current.leftButton.wasPressedThisFrame) {
+            if (Physics.Raycast(ray, out hit, interactionRange, whatIsShelf)) {
+
+                hit.collider.GetComponent<ShelfSpaceController>().PlaceStock(heldPickup);
+
+                if (heldPickup.isPlaced == true) {
+                    heldPickup = null;
+                }
+
+                if (AudioManager.instance != null) {
+                    AudioManager.instance.PlaySFX(7);
+                }
+            }
+        }
+
+        //if the stock is a card pack, open it.
+        if (Keyboard.current.fKey.wasPressedThisFrame) {
+            if (heldPickup.info != null && heldPickup.info.cardPack != null) {
+                Debug.Log("player pressed f with cardpack in hand");
+            }
+        }
+
+        // throw away whatever is in hand
+        if (Mouse.current.rightButton.wasPressedThisFrame) {
+            heldPickup.Release();
+            heldPickup.theRB.AddForce(theCam.transform.forward * throwForce, ForceMode.Impulse);
+
+
+            heldPickup.transform.SetParent(null);
+            heldPickup = null;
+
+            if (AudioManager.instance != null) {
+                AudioManager.instance.PlaySFX(9);
+            }
+        }
+    }
+
+    /// <summary>
+    /// Handles when you have a box of stock in your hand.
+    /// </summary>
+    private void HandleBoxInHand() {
+        Ray ray = theCam.ViewportPointToRay(new Vector3(.5f, .5f, 0f));
+        RaycastHit hit;
+
+        //placing stock onto a shelf
+        if (Mouse.current.leftButton.isPressed) {
+            placeStockCounter -= Time.deltaTime;
+
+            if (placeStockCounter <= 0) {
+                if (Physics.Raycast(ray, out hit, interactionRange, whatIsShelf)) {
+                    heldBox.PlaceStockOnShelf(hit.collider.GetComponent<ShelfSpaceController>());
+
+                    placeStockCounter = waitToPlaceStock;
+                }
+            }
+        }
+        
+        // throwing a box of stock
+        if (Mouse.current.rightButton.wasPressedThisFrame) {
+
+            heldBox.Release();
+            heldBox.theRB.AddForce(theCam.transform.forward * throwForce, ForceMode.Impulse);
+
+            heldBox.transform.SetParent(null);
+            heldBox = null;
+
+            if (AudioManager.instance != null) {
+                AudioManager.instance.PlaySFX(0);
+            }
+        }
+
+        // open and close the box
+        if (Keyboard.current.eKey.wasPressedThisFrame) {
+            heldBox.OpenClose();
+        }
+
+        // placing whatever is in the box onto a shelf
+        if (Mouse.current.leftButton.wasPressedThisFrame) {
+            if (heldBox.stockInBox.Count > 0) {
+
+                if (Physics.Raycast(ray, out hit, interactionRange, whatIsShelf)) {
+                    heldBox.PlaceStockOnShelf(hit.collider.GetComponent<ShelfSpaceController>());
+
+                    placeStockCounter = waitToPlaceStock;
 
                     if (AudioManager.instance != null) {
-                        AudioManager.instance.PlaySFX(5);
+                        AudioManager.instance.PlaySFX(7);
+                    }
+                }
+            } else {
+                // throwing away the box if it is empty
+                if (Physics.Raycast(ray, out hit, interactionRange, whatIsBin)) {
+                    Destroy(heldBox.gameObject);
+
+                    heldBox = null;
+
+                    if (AudioManager.instance != null) {
+                        AudioManager.instance.PlaySFX(10);
                     }
                 }
             }
 
+            
+        }
+        
+    }
+
+    /// <summary>
+    /// Handles when there is furniture in your hand.
+    /// </summary>
+    private void HandleFurnitureInHand() {
+        //grid snapping
+        Vector3 targetPos = furniturePoint.position;
+        heldFurniture.SetPlacementPosition(targetPos);
+
+        // rotating furniture by 30 degrees
+        float scroll = Mouse.current.scroll.ReadValue().y;
+        if (scroll > 0f) {
+            heldFurniture.RotatePlacement(1);
+        }
+        else if (scroll < 0f) {
+            heldFurniture.RotatePlacement(-1);
+        }
+
+        //checks if furniture is overlapping with an existing furniture
+        bool overlapping = heldFurniture.IsFurnitureOverlapping();
+
+        if (overlapping) {
+            heldFurniture.setColorRed();
+        } else {
+            heldFurniture.setColorGreen();
+        }
+
+        // placing the furniture
+        if (Mouse.current.leftButton.wasPressedThisFrame ||
+            Keyboard.current.rKey.wasPressedThisFrame) {
+
+            if (!overlapping) {
+                heldFurniture.transform.SetParent(null);
+                heldFurniture.PlaceFurniture();
+                heldFurniture = null;
+            }
+
+            if (AudioManager.instance != null) {
+                AudioManager.instance.PlaySFX(5);
+            }
         }
     }
     
